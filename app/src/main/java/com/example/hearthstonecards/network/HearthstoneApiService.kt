b20b8bba2021867ad4ea68bf404aa153528c53e8
mpalurik <m_palurik@utb.cz>
2024-10-31 15:39:12 +0100

    uprava API volani a pridani dalsich volani

--- app/src/main/java/com/example/hearthstonecards/network/HearthstoneApiService.kt
+++ app/src/main/java/com/example/hearthstonecards/network/HearthstoneApiService.kt
@@ -1,10 +1,25 @@
+import androidx.room.Query
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface HearthstoneApiService {
     @GET("cardbacks")
     suspend fun getCardBacks(
         @Header("X-RapidAPI-Key") apiKey: String
     ): Response<List<CardBack>>
+
+    @GET("cards/search/{cardName}/")
+    suspend fun searchCards(
+        @Header("X-RapidAPI-Key") apiKey: String,
+        @Path("cardName") cardName: String
+    ): Response<List<CardSearch>>
+
+    @GET("cards/{cardId}")
+    suspend fun getCardDetail(
+        @Header("X-RapidAPI-Key") apiKey: String,
+        @Path("cardId") cardId: String
+    ): Response<CardDetail>
+
 }
